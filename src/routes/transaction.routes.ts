aa4601bf2da650c14ebcf23de9cfc46e1432a77c
Leonardo Muller <leonaardo.muller@gmail.com>
2020-12-06 22:43:24 -0300

    Commit from 13:22

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
+import CreateTransactionService from '../services/CreateTransactionService';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+//import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
@@ -17,7 +18,20 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    //Pegar os parametros do corpo
+    const { title, value, type } = request.body
+    //Criar a transação que cuida da regra de negócio
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+    //Que vai criar a transação de fato e retorna-la
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type,
+    })
+
+    return response.json(transaction)
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
